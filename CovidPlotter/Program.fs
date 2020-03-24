d1621fd04b0f69aebbb3768a9faddd5a4f923940
squirrelyman <30965378+squirrelyman@users.noreply.github.com>
2020-03-23 20:28:22 -0400

    perform some basic curve fitting and plot multiple plots on a webpage

--- CovidPlotter/Program.fs
+++ CovidPlotter/Program.fs
@@ -2,19 +2,33 @@
 open System.IO
 open FSharp.Data
 open XPlot.Plotly
+open MathNet.Numerics.LinearAlgebra
+open MathNet.Numerics.LinearAlgebra.Solvers
 
 let dataDir = "../../../../COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/"
 
 type DailyData = CsvProvider<"../COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/02-01-2020.csv">
 
-let filterNonMonotonic (s: ('a * int) seq) =
+let fitLog2ndOrder (data: (float * float) seq) =
+    let mat = matrix ( data |> Seq.map(fun (t, x) -> [1.0; t; t*t]))
+    let vec = vector (data |> Seq.map(fun(t, x) -> Math.Log x))
+    [yield! mat.Solve(vec)]
+
+let fitLog1stOrder (data: (float * float) seq) =
+    let mat = matrix ( data |> Seq.map(fun (t, x) -> [1.0; t]))
+    let vec = vector (data |> Seq.map(fun(t, x) -> Math.Log x))
+    let res = new Double.DenseVector(Seq.length data)
+    [yield! mat.Solve(vec); 0.0]
+        
+
+let filterNonMonotonic (s: (float * 'b) seq) : (float * 'b) list =
     let maxVals = 
         [0..Seq.length s - 1]
         |> Seq.map(fun i -> s |> Seq.take(i+1) |> Seq.map(fun (t, _) -> t) |> Seq.max)
 
     [
         for ((t, num), maxSoFar) in Seq.zip s maxVals do
-            if t = maxSoFar
+            if maxSoFar - t < 0.001
             then yield (t, num)
     ]
 
@@ -63,10 +77,10 @@ let main argv =
         ("Iran", data |> Seq.where(fun x -> x.``Country/Region`` = "Iran"))
         ("United Kingdom", data |> Seq.where(fun x -> x.``Province/State`` = "United Kingdom"))
         ("India", data |> Seq.where(fun x -> x.``Country/Region`` = "India"))
-        ("South Korea", data |> Seq.where(fun x -> x.``Country/Region`` = "South Korea"))
+        ("South Korea", data |> Seq.where(fun x -> x.``Country/Region`` = "South Korea" || x.``Country/Region`` = "Korea, South"))
     ]
 
-    let buildPlot (data: (float * int) seq) =
+    let buildPlot (data: (float * float) seq) =
         Scatter(
             x = (data |> Seq.map(fun (x, y) -> (float)x)),
             y = (data |> Seq.map(fun (x, y) -> (float)y)),
@@ -78,7 +92,7 @@ let main argv =
     let casesSince100DayXY (data: DailyData.Row seq) =
         let dayOf100 = firstDayPast100 data
         data
-        |> Seq.map(fun x -> ((float)(x.``Last Update`` - dayOf100).TotalHours / 24.0, x.Confirmed))
+        |> Seq.map(fun x -> ((float)(x.``Last Update`` - dayOf100).TotalHours / 24.0, (float)x.Confirmed))
         |> Seq.where(fun (t, _) -> t >= 0.0)
         |> filterNonMonotonic
         |> buildPlot
@@ -86,44 +100,97 @@ let main argv =
     let deathsSince100DayXY (data: DailyData.Row seq) =
         let dayOf100 = firstDayPast100 data
         data
-        |> Seq.map(fun x -> ((float)(x.``Last Update`` - dayOf100).TotalHours / 24.0, x.Deaths))
+        |> Seq.map(fun x -> ((float)(x.``Last Update`` - dayOf100).TotalHours / 24.0, (float)x.Deaths))
         |> Seq.where(fun (t, _) -> t >= 0.0)
-        |> Seq.where(fun (_, x) -> x > 0)
+        |> Seq.where(fun (_, x) -> x > 0.0)
         |> filterNonMonotonic
         |> buildPlot
 
     let deathsVsConfirmedXY (data: DailyData.Row seq) =
         data
-        |> Seq.map(fun x -> (x.Confirmed, x.Deaths))
-        |> Seq.where(fun (c, d) -> c > 100 && d > 10)
-        |> Seq.map(fun (x, y) -> ((float)x, y))
+        |> Seq.map(fun x -> ((float)x.Confirmed, (float)x.Deaths))
+        |> Seq.where(fun (c, d) -> c > 100.0 && d > 10.0)
         |> buildPlot
 
-    dataByLocation
-    |> List.map(fun (loc, data) -> casesSince100DayXY data)
-    |> Chart.Plot
-    |> Chart.WithLabels(dataByLocation |> List.map(fun (loc, data) -> loc))
-    |> Chart.WithOptions(layoutLog)
-    |> Chart.WithXTitle("Days since cases exceeded 100")
-    |> Chart.WithYTitle("Confirmed Cases")
-    |> Chart.Show
+    [
+        dataByLocation
+        |> List.map(fun (loc, data) -> casesSince100DayXY data)
+        |> Chart.Plot
+        |> Chart.WithLabels(dataByLocation |> List.map(fun (loc, data) -> loc))
+        |> Chart.WithOptions(layoutLog)
+        |> Chart.WithXTitle("Days since cases exceeded 100")
+        |> Chart.WithYTitle("Confirmed Cases")
+
+        dataByLocation
+        |> List.map(fun (loc, data) -> deathsSince100DayXY data)
+        |> Chart.Plot
+        |> Chart.WithLabels(dataByLocation |> List.map(fun (loc, data) -> loc))
+        |> Chart.WithOptions(layoutLog)
+        |> Chart.WithXTitle("Days since cases exceeded 100")
+        |> Chart.WithYTitle("Deaths")
+
+        dataByLocation
+        |> List.map(fun (loc, data) -> deathsVsConfirmedXY data)
+        |> Chart.Plot
+        |> Chart.WithLabels(dataByLocation |> List.map(fun (loc, data) -> loc))
+        |> Chart.WithOptions(layoutLogLog)
+        |> Chart.WithXTitle("Confirmed Cases")
+        |> Chart.WithYTitle("Deaths")
+    ] |> Chart.ShowAll
 
     dataByLocation
-    |> List.map(fun (loc, data) -> deathsSince100DayXY data)
-    |> Chart.Plot
-    |> Chart.WithLabels(dataByLocation |> List.map(fun (loc, data) -> loc))
-    |> Chart.WithOptions(layoutLog)
-    |> Chart.WithXTitle("Days since cases exceeded 100")
-    |> Chart.WithYTitle("Deaths")
-    |> Chart.Show
+    |> Seq.map(fun (loc, data) ->
+        printfn "\n\n%s---------------------------------------" loc
+        let casesVsTime =
+            data
+            |> Seq.map(fun x -> ((float)(x.``Last Update`` - DateTime.Now).TotalHours / 24.0, (float)x.Confirmed))
+            |> Seq.where(fun (t, x) -> x > 50.0)
+            |> filterNonMonotonic
 
-    dataByLocation
-    |> List.map(fun (loc, data) -> deathsVsConfirmedXY data)
-    |> Chart.Plot
-    |> Chart.WithLabels(dataByLocation |> List.map(fun (loc, data) -> loc))
-    |> Chart.WithOptions(layoutLogLog)
-    |> Chart.WithXTitle("Confirmed Cases")
-    |> Chart.WithYTitle("Deaths")
-    |> Chart.Show
+        let casesToFit =
+            [
+                yield! casesVsTime
+                yield! casesVsTime |> List.rev |> List.take(3)
+                yield! casesVsTime |> List.rev |> List.take(2)
+                yield! casesVsTime |> List.rev |> List.take(1)
+                yield! casesVsTime |> List.rev |> List.take(3)
+                yield! casesVsTime |> List.rev |> List.take(2)
+                yield! casesVsTime |> List.rev |> List.take(1)
+                yield! casesVsTime |> List.rev |> List.take(3)
+                yield! casesVsTime |> List.rev |> List.take(2)
+                yield! casesVsTime |> List.rev |> List.take(1)
+            ]
+        let casesFit = casesToFit |> fitLog2ndOrder
+        let casesFit =
+            if casesFit.[2] < 0.0
+            then casesFit
+            else casesToFit |> fitLog1stOrder
+
+        printfn "Cases = e^((%f)t^2 + (%f)t + %f)" casesFit.[2] casesFit.[1] casesFit.[0]
+
+        let predictedCases = Math.Exp casesFit.[0]
+        let actualCases = casesVsTime |> Seq.map(fun (t, x) -> x) |> Seq.max
+        printfn "Current Predicted: %f Actual: %f" predictedCases actualCases
+        let dailyGrowth = Math.Exp casesFit.[1] - 1.0
+        let tDouble = Math.Log(2.0) / casesFit.[1]
+        printfn "Daily Growth: %f%% (doubles every %f days)" (dailyGrowth * 100.0) tDouble
+
+        let earliestInSeries = casesVsTime |> Seq.map(fun (t, _) -> t) |> Seq.min
+        [
+            casesVsTime;
+            [int earliestInSeries-1..7] |> List.map(float) |> List.map(fun t ->
+                (
+                    t,
+                    Math.Exp(casesFit.[2] * t * t + casesFit.[1] * t + casesFit.[0])
+            ))
+        ]
+        |> Seq.map(buildPlot)
+        |> Chart.Plot
+        |> Chart.WithLabels(["Actual"; "Fit"])
+        |> Chart.WithOptions(layoutLog)
+        |> Chart.WithYTitle("Confirmed Cases")
+        |> Chart.WithXTitle("time from present (days)")
+        |> Chart.WithTitle(loc)
+    ) |> Chart.ShowAll
 
     0 // return an integer exit code
